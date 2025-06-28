773468805793c2b3ab3ba4dbd769d81a95788ebc
PeterShin23 <pss9fp@virginia.edu>
2025-06-28 00:36:23 -0400

    Screen capture

--- cmd/assistant/main.go
+++ cmd/assistant/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/PeterShin23/MyAssistant/internal/screen"
 )
 
 // To Run: go run ./cmd/assistant
@@ -14,11 +15,17 @@ func main() {
 		Short: "MyAssistant CLI - your personal screen/audio capture tool",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("👋 Let's get to work!")
+
+			err := screen.CaptureScreenshot()
+			if err != nil {
+				fmt.Println("Failed to capture screenshot", err)
+				os.Exit(1)
+			}
 		},
 	}
 
 	if error := rootCmd.Execute(); error != nil {
-		fmt.Println("Error")
+		fmt.Println("Error", error)
 		os.Exit(1)
 	}
 }
